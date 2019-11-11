d421faa732cf3dd62c0eff785d7a5b4e094344ff
CarlosPumar <cpjimenez123@gmail.com>
2019-11-11 16:21:53 +0100

    Pagina principal p1

---
+++ epd5/p1/paginaPrincipal.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo '<nav>
+            <a href="index.php">Crear Aerolinea</a>
+            <a href="index2.php">Crear conexion</a>
+            <a href="index3.php">Informacion</a>
+        </nav>';
